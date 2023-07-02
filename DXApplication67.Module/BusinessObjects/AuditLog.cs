60c2ae84be131c1926c29f0f9feaa22a7f2a24fd
Kirsten Greed <kirsteng@jobtalk.com.au>
2023-07-03 08:11:16 +1000

    Add project files.

---
+++ DXApplication67.Module/BusinessObjects/AuditLog.cs
@@ -0,0 +1,15 @@
+﻿using DevExpress.Persistent.BaseImpl.EFCore.AuditTrail;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DXApplication67.Module.BusinessObjects
+{
+    [Table("AuditData")]
+    [DevExpress.Persistent.Base.NavigationItem("Logs")]
+    public class AuditLog : AuditDataItemPersistent
+    {
+
+
+        // public AuditLog() : base() { UserLogins = new ObservableCollection<ApplicationUserLoginInfo>(); }
+
+    }
+}
---
+++ DXApplication67.Module/BusinessObjects/DXApplication67DbContext.cs
@@ -0,0 +1,94 @@
+﻿using DevExpress.ExpressApp.EFCore.Updating;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using DevExpress.Persistent.BaseImpl.EF.PermissionPolicy;
+using DevExpress.Persistent.BaseImpl.EF;
+using DevExpress.ExpressApp.Design;
+using DevExpress.ExpressApp.EFCore.DesignTime;
+using DevExpress.Persistent.BaseImpl.EFCore.AuditTrail;
+
+namespace DXApplication67.Module.BusinessObjects;
+
+// This code allows our Model Editor to get relevant EF Core metadata at design time.
+// For details, please refer to https://supportcenter.devexpress.com/ticket/details/t933891.
+public class DXApplication67ContextInitializer : DbContextTypesInfoInitializerBase {
+	protected override DbContext CreateDbContext() {
+		var optionsBuilder = new DbContextOptionsBuilder<DXApplication67EFCoreDbContext>()
+            .UseSqlServer(";")
+            .UseChangeTrackingProxies()
+            .UseObjectSpaceLinkProxies();
+        return new DXApplication67EFCoreDbContext(optionsBuilder.Options);
+	}
+}
+//This factory creates DbContext for design-time services. For example, it is required for database migration.
+public class DXApplication67DesignTimeDbContextFactory : IDesignTimeDbContextFactory<DXApplication67EFCoreDbContext> {
+	public DXApplication67EFCoreDbContext CreateDbContext(string[] args) {
+		throw new InvalidOperationException("Make sure that the database connection string and connection provider are correct. After that, uncomment the code below and remove this exception.");
+		//var optionsBuilder = new DbContextOptionsBuilder<DXApplication67EFCoreDbContext>();
+		//optionsBuilder.UseSqlServer("Integrated Security=SSPI;Pooling=false;Data Source=(localdb)\\mssqllocaldb;Initial Catalog=DXApplication67");
+        //optionsBuilder.UseChangeTrackingProxies();
+        //optionsBuilder.UseObjectSpaceLinkProxies();
+		//return new DXApplication67EFCoreDbContext(optionsBuilder.Options);
+	}
+}
+[TypesInfoInitializer(typeof(DXApplication67ContextInitializer))]
+public class DXApplication67EFCoreDbContext : DbContext {
+	public DXApplication67EFCoreDbContext(DbContextOptions<DXApplication67EFCoreDbContext> options) : base(options) {
+	}
+	//public DbSet<ModuleInfo> ModulesInfo { get; set; }
+	public DbSet<ModelDifference> ModelDifferences { get; set; }
+	public DbSet<ModelDifferenceAspect> ModelDifferenceAspects { get; set; }
+	public DbSet<PermissionPolicyRole> Roles { get; set; }
+	public DbSet<DXApplication67.Module.BusinessObjects.ApplicationUser> Users { get; set; }
+    public DbSet<DXApplication67.Module.BusinessObjects.ApplicationUserLoginInfo> UserLoginInfos { get; set; }
+    public DbSet<AuditLog> AuditData { get; set; }
+    public DbSet<AuditEFCoreWeakReference> AuditEFCoreWeakReference { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.HasChangeTrackingStrategy(ChangeTrackingStrategy.ChangingAndChangedNotificationsWithOriginalValues);
+        modelBuilder.Entity<DXApplication67.Module.BusinessObjects.ApplicationUserLoginInfo>(b => {
+            b.HasIndex(nameof(DevExpress.ExpressApp.Security.ISecurityUserLoginInfo.LoginProviderName), nameof(DevExpress.ExpressApp.Security.ISecurityUserLoginInfo.ProviderUserKey)).IsUnique();
+        });
+        modelBuilder.Entity<AuditEFCoreWeakReference>()
+            .HasMany(p => p.AuditItems)
+            .WithOne(p => p.AuditedObject);
+        modelBuilder.Entity<AuditEFCoreWeakReference>()
+            .HasMany(p => p.OldItems)
+            .WithOne(p => p.OldObject);
+        modelBuilder.Entity<AuditEFCoreWeakReference>()
+            .HasMany(p => p.NewItems)
+            .WithOne(p => p.NewObject);
+        modelBuilder.Entity<AuditEFCoreWeakReference>()
+            .HasMany(p => p.UserItems)
+            .WithOne(p => p.UserObject);
+        modelBuilder.Entity<ModelDifference>()
+            .HasMany(t => t.Aspects)
+            .WithOne(t => t.Owner)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
+
+public class DXApplication67AuditingDbContext : DbContext {
+    public DXApplication67AuditingDbContext(DbContextOptions<DXApplication67AuditingDbContext> options) : base(options) {
+    }
+    public DbSet<AuditDataItemPersistent> AuditData { get; set; }
+    public DbSet<AuditEFCoreWeakReference> AuditEFCoreWeakReference { get; set; }
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder) {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.HasChangeTrackingStrategy(ChangeTrackingStrategy.ChangingAndChangedNotificationsWithOriginalValues);
+        modelBuilder.Entity<AuditEFCoreWeakReference>()
+            .HasMany(p => p.AuditItems)
+            .WithOne(p => p.AuditedObject);
+        modelBuilder.Entity<AuditEFCoreWeakReference>()
+            .HasMany(p => p.OldItems)
+            .WithOne(p => p.OldObject);
+        modelBuilder.Entity<AuditEFCoreWeakReference>()
+            .HasMany(p => p.NewItems)
+            .WithOne(p => p.NewObject);
+        modelBuilder.Entity<AuditEFCoreWeakReference>()
+            .HasMany(p => p.UserItems)
+            .WithOne(p => p.UserObject);
+    }
+}
